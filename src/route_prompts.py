30da4845149fdebfbe41220dcc87edbfc20aa4a8
biniyam69 <biniyam.ajaw@astu.edu.et>
2024-02-15 14:28:20 +0000

    image generation

---
+++ src/route_prompts.py
@@ -0,0 +1,8 @@
+import langchain
+from langchain.chains.router import MultiPromptChain
+from langchain.llms import OpenAI
+from langchain.chains import ConversationChain
+from langchain.chains.llm import LLMChain
+from langchain.prompts import PromptTemplate
+
+
